1eaa8a7c747259887685f885e0a9a813f22b9a40
Martin Ma <mma@pivotal.io>
2019-06-04 18:52:50 +0900

    added /get/<env> endpoint

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+    "os"
+//  "fmt"
+//  "strings"
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/hero"
+//	"github.com/kataras/iris/hero"
 )
 
 func main() {
     app := iris.Default()
-    app.Get("/ping", func(ctx iris.Context) {
+    port := os.Getenv("port")
+
+    app.Get("/", func(ctx iris.Context) {
         ctx.JSON(iris.Map{
-            "message": "pong",
+            "message": "Welcome to PCF KPI check",
         })
     })
+
+    os.Setenv("testenv", "abc123")
+    app.Get("/get/{name}", func(ctx iris.Context) {
+        name := ctx.Params().Get("name")
+        key := os.Getenv(name)
+
+        if len(key) == 0 {
+            ctx.Writef("Env variable not set")
+        } else {
+            ctx.Writef("%s=%s", name, key)
+        }
+    })
+
     // listen and serve on http://0.0.0.0:8080.
-    app.Run(iris.Addr(":8080"))
+    app.Run(iris.Addr(":"+port))
 }
