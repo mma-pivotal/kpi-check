b10b6b15fb4fd74ed1405b16dc94f195ddeaff83
Martin Ma <mma@pivotal.io>
2019-06-04 20:45:14 +0900

    add set ENV function

--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
     "os"
-//  "fmt"
-//  "strings"
+//    "encoding/json"
+//    "fmt"
+//    "strings"
 	"github.com/kataras/iris"
 //	"github.com/kataras/iris/hero"
 )
 
+type ENV struct {
+    Key string  `json:"key"`
+    Value string  `json:"value"`
+}
+
 func main() {
     app := iris.Default()
     port := os.Getenv("port")
+    if len(port) == 0 {
+        port = "8080"
+    }
+    //read port from ENV, default is 8080
 
-    app.Get("/", func(ctx iris.Context) {
+    app.Get("/", func(ctx iris.Context) { // root, may need to add FAQ link here
         ctx.JSON(iris.Map{
             "message": "Welcome to PCF KPI check",
         })
     })
 
-    os.Setenv("testenv", "abc123")
-    app.Get("/get/{name}", func(ctx iris.Context) {
+    app.Get("/get/{name}", func(ctx iris.Context) {  // get an ENV variable
         name := ctx.Params().Get("name")
         key := os.Getenv(name)
 
@@ -30,6 +39,24 @@ func main() {
         }
     })
 
-    // listen and serve on http://0.0.0.0:8080.
+    app.Post("/set", func(ctx iris.Context) {  // set an ENV variable
+        var kv ENV
+
+        if err := ctx.ReadJSON(&kv); err != nil{
+            ctx.StatusCode(iris.StatusBadRequest)
+            ctx.WriteString(err.Error())
+            return
+        } else {
+            err := os.Setenv(kv.Key, kv.Value)
+            if err != nil {
+                ctx.Writef("%s",err.Error())
+                return 
+            }
+            ctx.Writef("key: %s \n", kv.Key)
+            ctx.Writef("value: %s \n", kv.Value)
+        }
+    })
+
+    // listen and serve on http://0.0.0.0:<port>.
     app.Run(iris.Addr(":"+port))
 }
