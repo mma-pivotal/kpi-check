f62bd744f01aa6642d0f7648ed1cb2c5ff3445d8
mma-pivotal <mma@pivotal.io>
2019-06-05 11:45:33 +0900

    added delete endpoint

--- main.go
+++ main.go
@@ -1,64 +1,73 @@
 package main
 
 import (
-    "os"
-//    "encoding/json"
-//    "fmt"
-//    "strings"
+	"os"
+	//    "encoding/json"
+	//    "fmt"
+	//    "strings"
 	"github.com/kataras/iris"
-//	"github.com/kataras/iris/hero"
+	//	"github.com/kataras/iris/hero"
 )
 
 type ENV struct {
-    Key string  `json:"key"`
-    Value string  `json:"value"`
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
 func main() {
-    app := iris.Default()
-    port := os.Getenv("port")
-    if len(port) == 0 {
-        port = "8080"
-    }
-    //read port from ENV, default is 8080
+	app := iris.Default()
+	port := os.Getenv("port")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	//read port from ENV, default is 8080
 
-    app.Get("/", func(ctx iris.Context) { // root, may need to add FAQ link here
-        ctx.JSON(iris.Map{
-            "message": "Welcome to PCF KPI check",
-        })
-    })
+	app.Get("/", func(ctx iris.Context) { // root, may need to add FAQ link here
+		ctx.JSON(iris.Map{
+			"message": "Welcome to PCF KPI check",
+		})
+	})
 
-    app.Get("/get/{name}", func(ctx iris.Context) {  // get an ENV variable
-        name := ctx.Params().Get("name")
-        key := os.Getenv(name)
+	app.Get("/get/{name}", func(ctx iris.Context) { // get an ENV variable
+		name := ctx.Params().Get("name")
+		key := os.Getenv(name)
 
-        if len(key) == 0 {
-            ctx.Writef("Env variable not set")
-        } else {
-            ctx.Writef("%s=%s", name, key)
-        }
-    })
+		if len(key) == 0 {
+			ctx.Writef("Env variable not set")
+		} else {
+			ctx.Writef("%s=%s", name, key)
+		}
+	})
 
-    app.Post("/set", func(ctx iris.Context) {  // set an ENV variable
-        var kv ENV
+	app.Get("/delete/{name}", func(ctx iris.Context) { // get an ENV variable
+		name := ctx.Params().Get("name")
 
-        if err := ctx.ReadJSON(&kv); err != nil{
-            ctx.StatusCode(iris.StatusBadRequest)
-            ctx.WriteString(err.Error())
-            return
-        } else {
-            err := os.Setenv(kv.Key, kv.Value)
-            if err != nil {
-                ctx.Writef("%s",err.Error())
-                ctx.Writef("key: %s \n", kv.Key)
-                ctx.Writef("value: %s \n", kv.Value)
-                return 
-            }
-            ctx.Writef("key: %s \n", kv.Key)
-            ctx.Writef("value: %s \n", kv.Value)
-        }
-    })
+		if err := os.Unsetenv(name); err != nil {
+			ctx.StatusCode(iris.StatusBadRequest)
+			ctx.WriteString(err.Error())
+			return
+		}
+		ctx.Writef("%s has been removed", name)
+	})
 
-    // listen and serve on http://0.0.0.0:<port>.
-    app.Run(iris.Addr(":"+port))
+	app.Post("/set", func(ctx iris.Context) { // set an ENV variable
+		var kv ENV
+
+		if err := ctx.ReadJSON(&kv); err != nil {
+			ctx.StatusCode(iris.StatusBadRequest)
+			ctx.WriteString(err.Error())
+			return
+		} else {
+			err := os.Setenv(kv.Key, kv.Value)
+			if err != nil {
+				ctx.Writef("value: %s \n", kv.Value)
+				return
+			}
+			ctx.Writef("key: %s \n", kv.Key)
+			ctx.Writef("value: %s \n", kv.Value)
+		}
+	})
+
+	// listen and serve on http://0.0.0.0:<port>.
+	app.Run(iris.Addr(":" + port))
 }
